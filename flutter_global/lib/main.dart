0f68acebfbaf7911251cb087461bb011ff112465
arpan <arpanp99@gmail.com>
2019-06-01 15:39:25 +0530

    initial commit

---
+++ flutter_global/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_global/login.dart';
+
+void main()=>runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Flutter Community",
+    home: LoginPage()
+  )
+);
